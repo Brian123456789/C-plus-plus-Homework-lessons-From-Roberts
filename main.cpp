b9685acfecd21d56290ff1ec290ad0c8627c2c66
Brian123456789 <brianjpatterson@yahoo.com>
2017-02-03 23:05:12 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //int = integer, a number without a decimal place.
+    int tuna = 6;// tuna is a variable set to 6
+
+    cout << tuna << endl; // if you put tuna here with no quotes, it will output 6 on console
+    return 0;
+
+    //    note: it is ok to add values to variables right away, or later
+    //int a;
+    //a=24;
+    //or, int a = 24;
+}
