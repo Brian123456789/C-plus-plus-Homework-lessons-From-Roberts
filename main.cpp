76676613c2146db68da951ac3540baa691831de2
Brian123456789 <brianjpatterson@yahoo.com>
2017-02-03 23:24:48 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void printSomething(); // Prototype the function.
+
+//1. declare function
+//2. define the function
+//3. call the function
+
+
+int main()
+{
+    printSomething(); // Call the function.
+
+    return 0;
+}
+
+void printSomething(){ // Define the function.
+    cout << "oooo i am text ont he screen" << endl;
+}
