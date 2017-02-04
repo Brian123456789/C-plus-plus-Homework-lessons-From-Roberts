44da03466a077af5e82e2cdae4dd2531666fb21c
Brian123456789 <brianjpatterson@yahoo.com>
2017-02-03 23:22:24 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 98;
+    int b = 76;
+
+    if(a > b){ // Tests if a is greater than b.
+    // if (test) test parameter/condition {run code}
+
+        cout << "Bucky is AWESOME!!!!" << endl;
+    }//body uses curly brackets, code runs between them. They are "containers"
+
+    return 0;
+}
