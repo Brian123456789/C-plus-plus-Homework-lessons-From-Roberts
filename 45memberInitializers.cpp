9ee5f16ecce36459ee16b906d0d53e0660a0eae0
Brian123456789 <brianjpatterson@yahoo.com>
2017-02-05 01:00:12 -0800

    Add files via upload

---
+++ 45memberInitializers.cpp
@@ -0,0 +1,10 @@
+//This is main.cpp file
+#include <iostream>
+#include "Sally.h"
+using namespace std;
+
+int main()
+{
+	Sally so(3,87);
+	so.print();
+}
---
+++ 46_47composition.cpp
@@ -0,0 +1,15 @@
+//This is main.cpp file
+#include <iostream>
+#include "Birthday.h"
+#include "People.h"
+
+using namespace std;
+
+int main()
+{
+	Birthday birthObj(12,28,1986);
+
+	People buckyRoberts("Bucky the King",birthObj);
+	buckyRoberts.printInfo();
+
+}
